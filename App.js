6a88ab0ec0b09a3e9a4df5056a1fc01fe776bbbb
SB <sb@SARARATs-MacBook-Pro.local>
2021-05-03 19:57:05 -0400

    added google-font for manually use

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import HomeStack from './screens/homeStack';
 
+const getFonts = () => Font.loadAsync({
+  'JosefinSans': require('./assets/Josefin_Sans/JosefinSans-Regular.ttf'),
+});
+
 export default function App() {
-  return (
-    <HomeStack />
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded){
+    return(
+      <HomeStack />
+    );
+  }
+  else{
+    return(
+      <AppLoading 
+        startAsync = {getFonts}
+        onFinish = { () => setFontsLoaded(true) }
+        onError ={console.warn}
+      />
+    );
+  }
 }
