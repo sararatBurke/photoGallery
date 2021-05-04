53c4c72c32722ed9395674dab893929a5e0e7b0e
SB <sb@SARARATs-MacBook-Pro.local>
2021-05-03 23:25:25 -0400

    added toggle link

--- screens/homeStack.js
+++ screens/homeStack.js
@@ -1,19 +1,76 @@
-import React from 'react';
+import * as React from 'react';
+import { ScaledSheet } from 'react-native-size-matters';
 import { View, Text } from 'react-native';
-import { ScaledSheet} from 'react-native-size-matters';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+
+function Home({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.navText}>Home Screen</Text>
+      <Text style={styles.navText} title="Toggle" onPress={() => navigation.toggleDrawer()}>Toggle</Text>
+    </View>
+  );
+}
+
+function Gallery({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.navText}>Gallery Screen</Text>
+      <Text style={styles.navText} title="Toggle" onPress={() => navigation.toggleDrawer()}>Toggle</Text>
+    </View>
+  );
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Toggle"
+        onPress={() => props.navigation.toggleDrawer()}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator drawerStyle={{backgroundColor: '#fff',
+    width: 260}} drawerContent={props => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Gallery" component={Gallery} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-        <Text>Home</Text>
-    </View>
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
   );
 }
 
 const styles = ScaledSheet.create({
   container:{
     flex: 1,
-    padding: 10
-  }
+    alignItems: 'center', 
+    justifyContent: 'center'
 
+  },
+  navText: {
+    fontFamily: 'JosefinSans',
+    fontSize: 35
+  },
+  drawFont:{
+    
+  }
 })
