2003c39dc56cdc291f4ab0da3523796d3c0c6164
SB <sb@SARARATs-MacBook-Pro.local>
2021-05-03 23:22:07 -0400

    added more font

--- App.js
+++ App.js
@@ -5,6 +5,13 @@ import HomeStack from './screens/homeStack';
 
 const getFonts = () => Font.loadAsync({
   'JosefinSans': require('./assets/Josefin_Sans/JosefinSans-Regular.ttf'),
+  'JosefinSans-Italic': require('./assets/Josefin_Sans/JosefinSans-Italic.ttf'),
+  'JosefinSans-Light': require('./assets/Josefin_Sans/JosefinSans-Light.ttf'),
+  'JosefinSans-LightItalic': require('./assets/Josefin_Sans/JosefinSans-LightItalic.ttf'),
+  'JosefinSans-Medium': require('./assets/Josefin_Sans/JosefinSans-Medium.ttf'),
+  'JosefinSans-Bold': require('./assets/Josefin_Sans/JosefinSans-Bold.ttf'),
+  'JosefinSans-SemiBold': require('./assets/Josefin_Sans/JosefinSans-SemiBold.ttf'),
+  
 });
 
 export default function App() {
