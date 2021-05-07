b8178c2a5768f43ac5f189a2268a6deb561a6789
SB <sb@SARARATs-MacBook-Pro.local>
2021-05-06 20:13:45 -0400

    added home page content

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import { ScaledSheet } from 'react-native-size-matters';
-import { View, Text } from 'react-native';
+import { View, Text,  ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.homeContainer}>
-        <Text>Copyright ©2021 All rights reserved | This template is made with  by Colorlib</Text>
+        <View style={{justifyContent:'center', flex: 0.9, marginVertical: 20}}>
+            <ImageBackground
+            style={styles.image}
+            source={{
+            uri: 'https://images.unsplash.com/photo-1540206395-68808572332f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+            }}>
+            <View style={styles.content}>
+                <Text style={styles.imageText}>Nature</Text>
+                <Text style={styles.imageButton}>MORE PHOTOS</Text>
+            </View>
+        </ImageBackground>
+      </View>
+        <Text style={{fontFamily: 'JosefinSans-Medium', fontSize: 16, lineHeight: 25}}>Copyright ©2021 All rights reserved | This template is made with ♡ by <Text style={{color: '#20c997'}}>Colorlib</Text></Text>
     </View>
   );
 }
@@ -14,5 +26,31 @@ const styles = ScaledSheet.create({
     homeContainer:{
         flex: 1,
         padding: 15
+    },
+    image:{
+        flex:1,
+        resizeMode: 'stretch',
+    },
+    content:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageText:{
+        fontFamily: 'JosefinSans-Bold',
+        fontSize: 32,
+        color:'#fff'
+    }, 
+    imageButton: {
+        fontSize: 13,
+        fontFamily: 'JosefinSans-Bold',
+        color: '#fff',
+        borderWidth: 2, 
+        borderColor: '#fff',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        marginTop: 15
+        
     }
+
 })
