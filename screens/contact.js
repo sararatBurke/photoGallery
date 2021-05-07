54aee03a005142dfa80254b999267a812bc81ccc
SB <sb@SARARATs-MacBook-Pro.local>
2021-05-07 12:39:13 -0400

    added input section

--- screens/contact.js
+++ screens/contact.js
@@ -1,15 +1,62 @@
 import * as React from 'react';
 import { ScaledSheet } from 'react-native-size-matters';
-import { View, Text,  Image, ScrollView } from 'react-native';
+import { View, Text,  SafeAreaView, TextInput } from 'react-native';
 
 export default function App() {
+        const [fname, onChangeFname] = React.useState(null);
+        const [lname, onChangeLname] = React.useState(null);
+        const [email, onChangeEmail] = React.useState(null);
+        const [subject, onChangeSubject] = React.useState(null);
+        const [message, onChangeMessage] = React.useState(null);
+       
   return (
     <View style={styles.homeContainer}>
         <View style={styles.natureHeader}>
             <Text style={{fontFamily: 'JosefinSans-Medium', fontSize: 40,}}>Contact</Text>
             <Text style={{width:100, borderTopWidth: 2, borderTopColor: '#000', marginBottom: 10}}></Text>
         </View>
+        
 
+  
+    <SafeAreaView>
+      <Text style={styles.inputTitle}>First Name</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeFname}
+        value={fname}
+        placeholder="First Name"
+      />
+      <Text style={styles.inputTitle}>Last Name</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeLname}
+        value={lname}
+        placeholder="Last Name"
+      />
+      <Text style={styles.inputTitle}>Email</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeEmail}
+        value={email}
+        placeholder="Subject"
+      />
+      <Text style={styles.inputTitle}>Subject</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeSubject}
+        value={subject}
+        placeholder="Subject"
+      />
+      <Text style={styles.inputTitle}>Message</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeMessage}
+        value={message}
+        placeholder="Subject"
+      />
+      <Text style={styles.submit}>Send Message</Text>
+    </SafeAreaView>
+ 
         <View style={{marginTop: 10}}>
             <Text 
             style={{fontFamily: 'JosefinSans-Medium', fontSize: 16, lineHeight: 25, textAlign: 'center'}}>Copyright ©2021 All rights reserved | This template is made with ♡ by 
@@ -30,11 +77,28 @@ const styles = ScaledSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    gallery: {
-        flex: 0.9,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+    inputTitle: {
+        fontFamily: 'JosefinSans-SemiBold',
+        fontSize: 18,  
+        marginTop: 10
+    },
+    input: {
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#999999',
+        marginVertical: 10
+
+    },
+    submit: {
+        fontFamily: 'JosefinSans-Bold',
+        fontSize: 17,
+        color: '#fff',
+        textAlign: 'center',
+        padding: 15,
+        backgroundColor: '#20c997',
+        width: 170,
+        marginVertical: 10
+    },
    
 
 })
